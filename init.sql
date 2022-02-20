e8dd610e2cbc4f7b5cb4a81e2ac65990e8f8799f
Nicolas Hérault <nicolas.herault.imt@gmail.com>
2022-02-20 17:35:56 +0100

    Debug image pull error and refacto

--- init.sql
+++ init.sql
@@ -1,13 +1,13 @@
 BEGIN;
 
 CREATE USER dockeruser;
-CREATE DATABASE dockerdb;
+CREATE DATABASE postgresdb;
 
 CREATE TABLE USER (id_bin binary(16),
                    id_uuid DEFAULT gen_random_uuid() PRIMARY KEY,
                    name varchar(200));
 
-GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+GRANT ALL PRIVILEGES ON DATABASE postgresdb TO dockeruser;
 
 COMMIT;
 
